5e733ee778d85ab78d0a63529a3833fc1c653510
wellnahuel <nahuelcittadino@gmail.com>
2023-03-26 20:48:48 -0300

    reinstall tailwind, and unnistall chakraUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { ChakraProvider } from '@chakra-ui/react'
-
-import { Button, ButtonGroup } from '@chakra-ui/react'
-
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <ChakraProvider>
-      <div className="App bg-red-600">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="bg-slate-700">
-          <h1 class="bg-red-800">
-            Tailwind test
-          </h1>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
-      <Button colorScheme='blue'>ChakraUI Test button</Button>
-    </ChakraProvider>
+    <h1 className="text-3xl font-bold underline bg-slate-600">Hola</h1>
   )
 }
-
-export default App
