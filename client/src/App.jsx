5cd67591bb5e95f3387aca39931a4de308d310c5
wellnahuel <nahuelcittadino@gmail.com>
2023-03-26 21:14:10 -0300

    install chakraUI OK

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-slate-600">Hola</h1>
+    <ChakraProvider>
+
+      <h1 className="text-3xl font-bold underline bg-slate-600">Hola</h1>
+      <Button colorScheme='blue' className='underline'>Button</Button>
+
+    </ChakraProvider>
   )
 }
